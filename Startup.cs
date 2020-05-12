579c91a5f95ff3360eee32d19376e7dee23b5b21
Pedro Torres Paredes <pedris11s.dev@gmail.com>
2020-05-12 19:45:27 -0400

    adding cors

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,17 @@ namespace TelegramBotNotifierApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opts =>
+            {
+                opts.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin();
+                    builder.AllowCredentials();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyHeader();
+                });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TelegramBotNotifierApi", Version = "v1", Contact = new OpenApiContact{
@@ -52,6 +63,8 @@ namespace TelegramBotNotifierApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("AllowAll");
 
             app.UseSwagger();
             app.UseSwaggerUI(c=>    
